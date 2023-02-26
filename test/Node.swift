a67421c0b0050ca999565b1266a2b6c0d8e2b2b4
novexex <104654115+novexex@users.noreply.github.com>
2023-02-26 09:08:52 +0300

    через раз работает сохранение и не удаляет ноды после пулла с юзердефолс

--- test/Node.swift
+++ test/Node.swift
@@ -16,13 +16,11 @@ class Node: Codable, Identifiable, ObservableObject, Hashable {
         hasher.combine(id)
     }
 
-    let id: UUID
     @Published var name: String
     @Published var children: [Node]
     weak var parent: Node?
 
     init(name: String = "", children: [Node] = [], parent: Node? = nil) {
-        self.id = UUID()
         self.name = name
         self.children = children
         self.parent = parent
@@ -33,14 +31,12 @@ class Node: Codable, Identifiable, ObservableObject, Hashable {
     }
 
     enum CodingKeys: String, CodingKey {
-        case id
         case name
         case children
     }
 
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.id = try container.decode(UUID.self, forKey: .id)
         self.name = try container.decode(String.self, forKey: .name)
         self.children = try container.decode([Node].self, forKey: .children)
         self.parent = nil
@@ -48,7 +44,6 @@ class Node: Codable, Identifiable, ObservableObject, Hashable {
 
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(id, forKey: .id)
         try container.encode(name, forKey: .name)
         try container.encode(children, forKey: .children)
     }
